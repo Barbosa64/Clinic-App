7151073076174c3a72a89400da2eff5b1aa181eb
Barbosa64 <rui.barbosa64@gmail.com>
2025-05-10 02:52:17 +0100

    auth update

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import PatientsLista from './pages/admin/Patients';
 import ScheduleAppointment from './pages/_ScheduleAppointment';
+import { useAuth } from './contexts/authContext';
+import Login from './pages/login';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
+			<Login />
 			<Navbar />
 			<PatientsLista />
 			<Home />
