b5e019b240ea1efe7d9044f0a65d07103277fcda
Barbosa64 <rui.barbosa64@gmail.com>
2025-05-13 21:44:55 +0100

    auth-menu-update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,56 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Navbar from './layouts/Navbar';
 import PatientsLista from './pages/admin/Patients';
-import ScheduleAppointment from './pages/_ScheduleAppointment';
+import ScheduleAppointment from './pages/admin/ScheduleAppointment';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import AuthRoute from './pages/AuthRoute';
 
 function App() {
-	//const [count, setCount] = useState(0);
+	const location = useLocation();
 
 	return (
 		<>
-			<Navbar />
-			<PatientsLista />
-			<ScheduleAppointment />
+			{location.pathname !== '/login' && location.pathname !== '/signup' && <Navbar />}
+
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				<Route path='/signup' element={<Signup />} />
+
+				<Route
+					path='/'
+					element={
+						<AuthRoute>
+							<PatientsLista />
+						</AuthRoute>
+					}
+				/>
+				<Route
+					path='/pacientes'
+					element={
+						<AuthRoute>
+							<PatientsLista />
+						</AuthRoute>
+					}
+				/>
+				<Route
+					path='/marcar-consulta'
+					element={
+						<AuthRoute>
+							<ScheduleAppointment />
+						</AuthRoute>
+					}
+				/>
+
+				{/* fallback 404 */}
+				<Route path='*' element={<Navigate to='/' replace />} />
+			</Routes>
 		</>
 	);
 }
 
-export default App;
+export default () => (
+	<Router>
+		<App />
+	</Router>
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './styles/global.css';
 import './lib/firebase';
-import AuthRoute from './pages/AuthRoute';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<Router>
-			<Routes>
-				<Route
-					path='/'
-					element={
-						<AuthRoute>
-							<App />
-						</AuthRoute>
-					}
-				/>
-				<Route path='/login' element={<Login />} />
-				<Route path='/signup' element={<Signup />} />
-
-				<Route path='*' element={<Navigate to='/' />} />
-			</Routes>
-		</Router>
+		<App />
 	</React.StrictMode>,
 );
-
-/*createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
-*/
