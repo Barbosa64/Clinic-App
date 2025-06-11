f855d238eeb36d000204bc5bf459aee3541c8cf3
Barbosa64 <rui.barbosa64@gmail.com>
2025-06-11 17:37:09 +0100

    Signup fixed

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -11,7 +11,7 @@ const Signup = () => {
 	const [insurance, setInsurance] = useState('');
 	const [insuranceNumber, setInsuranceNumber] = useState('');
 	const [imageUrl, setImageUrl] = useState('');
-	const [gender, setGender] = useState<'male' | 'female' | 'other'>('other');
+	const [gender, setGender] = useState<'Masculino' | 'Feminino' | 'Outro'>('Outro');
 	const [phone, setPhone] = useState('');
 	const [birthDate, setBirthDate] = useState('');
 	const [age, setAge] = useState('');
@@ -22,6 +22,10 @@ const Signup = () => {
 
 	const handleSignup = async () => {
 		setError('');
+		if (!name || !email || !password || !phone || !birthDate || !age) {
+			setError('Por favor, preencha todos os campos obrigatórios.');
+			return;
+		}
 		try {
 			const { user } = await createUserWithEmailAndPassword(auth, email, password);
 
@@ -48,10 +52,10 @@ const Signup = () => {
 	return (
 		<div className='w-full h-screen flex'>
 			{/* Seção Esquerda (ilustração ou cor de fundo) */}
-			<div className='w-1/2 h-full flex flex-col bg-[#282c34]' items-center justify-center></div>
+			<div className="w-1/2 h-full flex flex-col items-center justify-center bg-[url('../assets/17818.jpg')] bg-cover bg-center"></div>
 
 			{/* Seção Direita (formulário) */}
-			<div className='w-1/2 h-full bg-[#1a1a1a] flex flex-col p-20 justify-center'>
+			<div className='w-1/2 h-full bg-teal-600 flex flex-col p-20 justify-center'>
 				<div className='w-full flex flex-col max-w-[450px] mx-auto'>
 					<div className='w-full flex flex-col mb-10 text-white'>
 						<h3 className='text-lg mb-4'>Crie a sua conta para começar.</h3>
@@ -62,74 +66,80 @@ const Signup = () => {
 						<input
 							type='text'
 							placeholder='Nome'
-							className='w-full bg-transparent text-lg placeholder:text-white border-b border-white py-4 mb-4 outline-none'
+							className='w-full bg-transparent text-lg placeholder:text-white border-b text-white border-white py-4 mb-4 outline-none'
 							value={name}
 							onChange={e => setName(e.target.value)}
+							required
 						/>
 						<input
 							type='email'
 							placeholder='Email'
-							className='w-full bg-transparent text-lg placeholder:text-white border-b border-white py-4 mb-4 outline-none'
+							className='w-full bg-transparent text-lg placeholder:text-white border-b text-white border-white py-4 mb-4 outline-none'
 							value={email}
 							onChange={e => setEmail(e.target.value)}
+							required
 						/>
 						<input
 							type='password'
 							placeholder='Senha'
-							className='w-full bg-transparent text-lg placeholder:text-white border-b border-white py-4 mb-4 outline-none'
+							className='w-full bg-transparent text-lg placeholder:text-white border-b text-white border-white py-4 mb-4 outline-none'
 							value={password}
 							onChange={e => setPassword(e.target.value)}
+							required
 						/>
 						<input
 							type='text'
 							placeholder='Telefone'
-							className='w-full bg-transparent text-lg placeholder:text-white border-b border-white py-4 mb-4 outline-none'
+							className='w-full bg-transparent text-lg placeholder:text-white border-b text-white border-white py-4 mb-4 outline-none'
 							value={phone}
 							onChange={e => setPhone(e.target.value)}
+							required
 						/>
 						<input
 							type='date'
 							placeholder='Data de Nascimento'
-							className='w-full bg-transparent text-lg placeholder:text-white border-b border-white py-4 mb-4 outline-none'
+							className='w-full bg-transparent text-lg placeholder:text-white border-b text-white border-white py-4 mb-4 outline-none'
 							value={birthDate}
 							onChange={e => setBirthDate(e.target.value)}
+							required
 						/>
 						<input
 							type='number'
 							placeholder='Idade'
-							className='w-full bg-transparent text-lg placeholder:text-white border-b border-white py-4 mb-4 outline-none'
+							className='w-full bg-transparent text-lg placeholder:text-white border-b text-white border-white py-4 mb-4 outline-none'
 							value={age}
 							onChange={e => setAge(e.target.value)}
+							required
 						/>
 						<input
 							type='text'
 							placeholder='Seguro'
-							className='w-full bg-transparent text-lg placeholder:text-white border-b border-white py-4 mb-4 outline-none'
+							className='w-full bg-transparent text-lg placeholder:text-white border-b text-white border-white py-4 mb-4 outline-none'
 							value={insurance}
 							onChange={e => setInsurance(e.target.value)}
 						/>
 						<input
 							type='text'
 							placeholder='Número do Seguro'
-							className='w-full bg-transparent text-lg placeholder:text-white border-b border-white py-4 mb-4 outline-none'
+							className='w-full bg-transparent text-lg placeholder:text-white border-b text-white border-white py-4 mb-4 outline-none'
 							value={insuranceNumber}
 							onChange={e => setInsuranceNumber(e.target.value)}
 						/>
 						<input
 							type='url'
 							placeholder='URL da Imagem'
-							className='w-full bg-transparent text-lg placeholder:text-white border-b border-white py-4 mb-4 outline-none'
+							className='w-full bg-transparent text-lg placeholder:text-white border-b text-white border-white py-4 mb-4 outline-none'
 							value={imageUrl}
 							onChange={e => setImageUrl(e.target.value)}
 						/>
-						<select value={gender} onChange={e => setGender(e.target.value as 'male' | 'female' | 'other')} className='bg-transparent border border-white text-white py-3 px-2 rounded mt-2'>
-							<option className='bg-black' value='male'>
+						<select value={gender} onChange={e => setGender(e.target.value as 'Maculino' | 'Feminino' | 'Outro')} className='bg-transparent border border-white text-white py-3 px-2 rounded mt-2'>
+							<option className='bg-black' value='Masculino'>
 								Masculino
 							</option>
-							<option className='bg-black' value='female'>
+							<option className='bg-black' value='Feminino'>
 								Feminino
 							</option>
-							<option className='bg-black' value='other'>
+							<option className='bg-black' value='Outro'>
 								Outro
 							</option>
 						</select>
